4083c4eaa7ec1f6c01ba314ade6051e30db94cd4
unknown <diebraga0055@gmail.com>
2020-08-06 13:32:05 +0100

    added addtask route and template

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
 
 app.run(debug=True)
