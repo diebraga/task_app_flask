66ef4673195da7f7ec87031874224cb961eee8a2
unknown <diebraga0055@gmail.com>
2020-08-08 13:11:44 +0100

    sending form + sumbit button

--- app.py
+++ app.py
@@ -23,5 +23,12 @@ def get_tasks():
 def add_task():
     return render_template('addtask.html', categories=mongo.db.categories.find())
 
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+
+
 
 app.run(debug=True)
